4da387a60d3212387e42eefd3af741af1e302f49
TheBigEye <nahuelsenek@gmail.com>
2021-02-07 15:35:55 -0300

    Revert "Temporarily disable the update system"
    This reverts commit d23ee8f71f08f7af4db3f7c376bb8dd61d5010e2.

--- src/minicraft/core/Network.java
+++ src/minicraft/core/Network.java
@@ -38,7 +38,7 @@ public class Network extends Game {
 			// fetch the latest version from github
 			if(debug) System.out.println("Fetching release list from github...");
 			try {
-				HttpResponse<JsonNode> response = Unirest.get("https://api.github.com/repos/chrisj42/minicraft-plus-revived/releases").asJson();
+				HttpResponse<JsonNode> response = Unirest.get("google.com").asJson();
 				//HttpResponse<JsonNode> response = Unirest.get("https://api.github.com/repos/TheBigEye/Cthulhucraft/releases").asJson();
 				if(response.getStatus() != 200) {
 					System.err.println("Version request returned status code "+response.getStatus()+": "+response.getStatusText());
